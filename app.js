8453696d32e3a9ad8ddab1824424a0965c42b50d
NeerajVodala <vodalaneeraj@gmail.com>
2021-09-06 17:17:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const inputDate = document.querySelector(".input-bday");
 const checkButton = document.querySelector(".btn-check");
 const output = document.querySelector(".result")
 
-
-
-
-
-
-
 checkButton.addEventListener("click", function palindromeCheck() {
     
 })
